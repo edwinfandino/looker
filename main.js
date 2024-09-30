1d3acb8b3480362a6db44c65c5f2a045bf91d132
edwinfandino <107560206+edwinfandino@users.noreply.github.com>
2024-09-30 16:22:44 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const container = document.createElement('div');
+container.innerHTML = `
+  <label style="font-family: Roboto; font-size: 14px; margin-bottom: 5px;">Observaciones</label>
+  <textarea id="observaciones" rows="6" style="width: 100%; font-family: Roboto; font-size: 12px; padding: 10px; border: 1px solid #CCCCCC; border-radius: 4px;">
+  </textarea>
+`;
+document.body.appendChild(container);
